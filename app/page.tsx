e2de9e22319b7ad4cdde5c1b28e9cb12a10ad588
Lukas Tomasek <vedstav31@gmail.com>
2024-10-27 18:55:36 -0400

    code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import {
- DatacommButton,
- ButtonTypes,
-} from './components/buttons/datacomm-button';
-
 export default function Home() {
  return <main className="flex flex-col justify-center items-center m-5"></main>;
 }
