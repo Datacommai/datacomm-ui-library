dfb841deaf25ead66405772705afb11dd115d1ae
Lukas Tomasek <vedstav31@gmail.com>
2024-10-27 18:55:14 -0400

    code cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,5 @@ import {
 } from './components/buttons/datacomm-button';
 
 export default function Home() {
- return (
-  <main className="flex flex-col justify-center items-center m-5">
-   <h1>Datacomm UI Library</h1>
-   <section className="m-5">
-    <DatacommButton type={ButtonTypes.PRIMARY} title="sign up" />
-   </section>
-  </main>
- );
+ return <main className="flex flex-col justify-center items-center m-5"></main>;
 }
