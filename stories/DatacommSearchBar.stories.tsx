57a7f2a7ab616ff02365fa93955411d6b2afb38f
Your Name <you@example.com>
2024-11-10 20:17:59 -0700

    #30 - added oninput to storybook

--- stories/DatacommSearchBar.stories.tsx
+++ stories/DatacommSearchBar.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { DatacommSearchbar } from "../app/components/search-bar/datacomm-search-bar";
+import { action } from "@storybook/addon-actions";
 
 const meta: Meta<typeof DatacommSearchbar> = {
   title: "Datacomm/SearchBar",
@@ -13,5 +14,9 @@ type Story = StoryObj<typeof DatacommSearchbar>;
 export const Searchbar: Story = {
   args: {
     placeholder: "Type here...",
+    onInput: (inputValue: string) => {
+      console.log(inputValue);
+      action("onInput")(inputValue);
+    },
   },
 };
