4b2b96c2261e32fafe865b5488bb402d60ae0a0a
Your Name <you@example.com>
2024-11-18 09:03:03 -0700

    #36 - added missing user prop in storybook

--- stories/DatacommChatbotMessages.stories.tsx
+++ stories/DatacommChatbotMessages.stories.tsx
@@ -2,10 +2,9 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { DatacommChatbotMessage } from "../app/components/chatbot-messages/datacomm-chatbot-messages";
 import { action } from "@storybook/addon-actions";
 
-// Define the meta configuration for the Storybook story
 const meta: Meta<typeof DatacommChatbotMessage> = {
-  title: "Datacomm/Chatbot Messages", // Storybook title (category)
-  component: DatacommChatbotMessage, // Component being showcased
+  title: "Datacomm/Chatbot Messages",
+  component: DatacommChatbotMessage,
 };
 
 export default meta;
@@ -14,6 +13,7 @@ type Story = StoryObj<typeof DatacommChatbotMessage>;
 
 export const ChatbotMessages: Story = {
   args: {
+    user: "Person",
     text: "Please start the process of Mr. Michel Emrad onboarding, send them a welcome email, and schedule mail for tomorrow about reminding them.",
 
     isGoodFeedback: (feedback: boolean) => {
